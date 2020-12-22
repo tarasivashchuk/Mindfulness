0c0ffb6103b00b1d694b436314f5f0f52b014ec6
GitHub Action <action@github.com>
2020-12-22 01:45:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/tarasivashchuk/mindfulness/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tarasivashchuk/mindfulness/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tarasivashchuk.mindfulness.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tarasivashchuk.mindfulness.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/tarasivashchuk/mindfulness/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tarasivashchuk.mindfulness.services
+
+import com.github.tarasivashchuk.mindfulness.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
